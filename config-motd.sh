3a31ae95bc0013814fced23a0218b5fa93fb0226
James Bailey <jbailey@codisms.com>
2019-02-12 13:05:29 -0800

    Alter motd setup

--- config-motd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SUDO=$(which sudo 2> /dev/null)
-
-echo -e "\e[35mSetting up motd...\e[0m"
-[ -f /etc/motd ] && $SUDO mv /etc/motd /etc/motd.orig
-$SUDO ln -s ${HOME}/.dotfiles/motd /etc/motd
