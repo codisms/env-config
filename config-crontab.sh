0d1377d51e65baf8e25e88c2f6cf031cda93c714
James Bailey <jbailey@codisms.com>
2022-02-25 15:13:08 +0000

    - Add websocat to bin - Fix logging for crontab scripts - Configure git to use vimdiff for merges

--- config-crontab.sh
+++ config-crontab.sh
@@ -2,6 +2,6 @@
 
 echo "Add cron jobs..."
 (crontab -u ${USER} -l 2>/dev/null; \
-	echo "0 5 * * * ${HOME}/.dotfiles/bin/crontab-daily"; \
-	echo "5 5 * * 1 ${HOME}/.dotfiles/bin/crontab-weekly"; \
-	echo "15 5 1 * * ${HOME}/.dotfiles/bin/crontab-monthly") | crontab -u ${USER} -
+	echo "0 5 * * * ${HOME}/.dotfiles/bin/crontab-daily 2>&1 | sudo tee -a /var/log/crontab-daily.log"; \
+	echo "5 5 * * 1 ${HOME}/.dotfiles/bin/crontab-weekly 2>&1 | sudo tee -a /var/log/crontab-weedly.log"; \
+	echo "15 5 1 * * ${HOME}/.dotfiles/bin/crontab-monthly 2>&1 | sudo tee -a /var/log/crontab-monthly.log") | crontab -u ${USER} -
