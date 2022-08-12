9d17b980d9a7f6405974f550e028d6d3ef1aadf5
James Bailey <codisms@users.noreply.github.com>
2022-08-12 14:42:23 -0700

    Update config-crontab.sh
    Fix spelling mistake

--- config-crontab.sh
+++ config-crontab.sh
@@ -3,5 +3,5 @@
 echo "Add cron jobs..."
 (crontab -u ${USER} -l 2>/dev/null; \
 	echo "0 5 * * * ${HOME}/.dotfiles/bin/crontab-daily 2>&1 | sudo tee -a /var/log/crontab-daily.log"; \
-	echo "5 5 * * 1 ${HOME}/.dotfiles/bin/crontab-weekly 2>&1 | sudo tee -a /var/log/crontab-weedly.log"; \
+	echo "5 5 * * 1 ${HOME}/.dotfiles/bin/crontab-weekly 2>&1 | sudo tee -a /var/log/crontab-weekly.log"; \
 	echo "15 5 1 * * ${HOME}/.dotfiles/bin/crontab-monthly 2>&1 | sudo tee -a /var/log/crontab-monthly.log") | crontab -u ${USER} -
